e21269067676a3d80f1c1b6bd5fcf5a1b3f2999f
xiachunqiu <529613857@qq.com>
2018-11-23 16:42:11 +0800

    .

--- service-feign/src/main/java/com/x/y/servicehi/controller/HiController.java
+++ service-feign/src/main/java/com/x/y/servicehi/controller/HiController.java
@@ -4,7 +4,6 @@ import com.x.y.servicehi.service.SchedualServiceHi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
